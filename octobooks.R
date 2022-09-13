0407b8d6e3c0c6c20411cda0af258dfb3ca81af7
Eliot Forcadell <eliot.forcadell@gmail.com>
2022-09-14 00:37:49 +0200

    Add pacman package installation

--- octobooks.R
+++ octobooks.R
@@ -3,7 +3,7 @@
 # Eliot Forcadell
 # 2022/09/13
 #########################################################
-
+install.packages("pacman")
 pacman::p_load(shiny, shinyjs, shinyWidgets, DT, yaml,
                htmltools, rvest, httr, RSelenium, curl,
                data.table, stringr, lubridate, tools,
